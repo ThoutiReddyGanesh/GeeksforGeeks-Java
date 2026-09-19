6295d23ca7c44f05e948cfa19dde5002f8178f22
ThoutiReddyGanesh <ganeshthoutireddy@gmail.com>
2026-09-19 10:35:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isBalanced(String s) {
+        // code here
+        while(s.contains("()")||s.contains("[]")||s.contains("{}")){
+
+                    s=s.replace("()","");
+                    s=s.replace("[]","");
+                    s=s.replace("{}","");
+                }
+
+                if(s.length()==0) return true;
+                return false;
+            }
+        }
